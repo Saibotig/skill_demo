a13e560c0a78df54bc740427261d81795217ae1c
Saibotig <tobias.baumann13@gmx.de>
2023-02-23 11:01:10 +0100

    added requested changes

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//function to print random numbers
+/* function to print random numbers */
 void printRandom(int lower, int upper, int n) {
 	for (int i = 0; i < n; i++) {
 		int rnd = (rand() % (upper + lower)) - lower;
