a78ec91b70fbd1f227bf355eaa9f4c98845ea23a
Saibotig <tobias.baumann13@gmx.de>
2023-02-23 09:27:18 +0100

    added printRandom function

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+
+//function to print random numbers
+void printRandom(int lower, int upper, int n) {
+	for (int i = 0; i < n; i++) {
+		int rnd = (rand() % (upper + lower)) - lower;
+		printf("%d", rnd);
+	}
+}
+
 int main() {
+	printRandom(20, 333, 12);
 	return 0;
 }
